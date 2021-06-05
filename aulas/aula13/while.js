2ba12f1f717cc40c0f5df2a0fcf34cf1c5691722
MariaAlice00 <mariaalice021200@gmail.com>
2021-06-05 09:23:10 -0300

    repetições

---
+++ aulas/aula13/while.js
@@ -0,0 +1,12 @@
+console.log('A pizza tem 6 fatias.')
+
+quantFatias = 6
+contador = 0
+
+while (quantFatias > 0) {
+    contador++
+    console.log(`Comi a fatia ${contador}`)
+    quantFatias--
+}
+
+console.log('Terminei a pizza!')
